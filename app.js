0829056a2dc6f6f67c02a3990cc35464702f2014
Filip Lizoń <filip.lizon1@gmail.com>
2021-05-09 18:15:40 +0200

    add settings for requesting data from forms

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", routes);
 
 module.exports = app;
