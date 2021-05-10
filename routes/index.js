30f35569afb9fdbbdaffbb0332e8ebc7b34cf1e4
Filip Lizoń <filip.lizon1@gmail.com>
2021-05-10 16:29:55 +0200

    import controllers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("app");
-});
+const PagesController = require("../controllers/PagesController");
+const ApplicationController = require("../controllers/ApplicationController");
+
+router.get("/", PagesController.home);
+
+router.get("/read", ApplicationController.read);
+
+router.post("/save", ApplicationController.save);
 
 module.exports = router;
